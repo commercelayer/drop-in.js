72bfcd900a33db1e4ba9836c706200aa0b669ac1
Marco Montalbano <marco.montalbano@commercelayer.io>
2022-09-26 20:53:16 +0200

    chore: add global style

--- packages/drop-in/stencil.config.ts
+++ packages/drop-in/stencil.config.ts
@@ -2,9 +2,10 @@ import { Config } from '@stencil/core'
 
 export const config: Config = {
   namespace: 'drop-in',
-  preamble: 'MIT License\nCopyright (c) 2022 Commerce Layer',
+  // preamble: 'MIT License\nCopyright (c) 2022 Commerce Layer',
 
   globalScript: './src/global/app.ts',
+  globalStyle: './src/global/app.scss',
 
   minifyJs: true,
   minifyCss: true,
