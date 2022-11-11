9cd665572bab6276948a0397043c764126c3f826
Marco Montalbano <marco.montalbano@commercelayer.io>
2022-11-11 12:39:53 +0100

    feat: add separate css file for minicart

--- packages/drop-in/stencil.config.ts
+++ packages/drop-in/stencil.config.ts
@@ -7,7 +7,7 @@ export const config: Config = {
   namespace: 'drop-in',
   // preamble: 'MIT License\nCopyright (c) 2022 Commerce Layer',
 
-  globalStyle: './src/global/app.scss',
+  globalStyle: './src/styles/drop-in.scss',
 
   buildEs5: false,
 
