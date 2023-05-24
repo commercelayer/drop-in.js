70061feec0dbc09bbcfe658e547c26bdc6be8e36
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-05-24 17:56:03 +0200

    docs: update css addon titles

--- packages/docs/.storybook/addon-drop-in-css/manager.tsx
+++ packages/docs/.storybook/addon-drop-in-css/manager.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { addons, types } from '@storybook/addons'
-import { ADDON_ID, ADDON_NAME } from './constants'
+import { ADDON_ID, ADDON_TITLE } from './constants'
 import { Tool } from './Tool'
 
 addons.register(ADDON_ID, () => {
   addons.add(ADDON_ID, {
-    title: ADDON_NAME,
+    title: ADDON_TITLE,
     type: types.TOOL,
     match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
     render: () => <Tool />,
