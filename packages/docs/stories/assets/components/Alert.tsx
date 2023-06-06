f9384f0367eed8f381d0c0f5eb13a60febd9de05
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-06-06 17:11:36 +0200

    chore: remove 'key' error in console

--- packages/docs/stories/assets/components/Alert.tsx
+++ packages/docs/stories/assets/components/Alert.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
 
-/*
-<div className="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4" role="alert">
-  <p className="!m-0 font-bold">Info</p>
-  <p className="!m-0 ">The pre-filled code refers to an available product, try to change it with an out-of-stock (e.g. <code>{codes.outOfStock}</code>) and see how the component behaves in the example above.</p>
-</div> 
-*/
 interface Props {
   title: string
   type: 'info' | 'danger' | 'warning' | 'success'
@@ -22,19 +16,29 @@ export const Alert: React.FC<Props> = ({ title, type = 'info', children }) => {
       ? 'bg-orange-100 border-orange-500 text-orange-700'
       : 'bg-blue-100 border-blue-500 text-blue-700'
 
-  const titleElement = React.createElement('p', {
-    className: '!m-0 font-bold',
-    children: title
-  })
+  const titleElement = React.createElement(
+    'p',
+    {
+      className: '!m-0 font-bold'
+    },
+    title
+  )
 
-  const contentElement = React.createElement('div', {
-    className: '[&>p]:!m-0',
+  const contentElement = React.createElement(
+    'div',
+    {
+      className: '[&>p]:!m-0'
+    },
     children
-  })
+  )
 
-  return React.createElement('div', {
-    role: 'alert',
-    className: `border-l-4 p-4 ${color}`,
-    children: [titleElement, contentElement]
-  })
+  return React.createElement(
+    'div',
+    {
+      role: 'alert',
+      className: `border-l-4 p-4 ${color}`
+    },
+    titleElement,
+    contentElement
+  )
 }
