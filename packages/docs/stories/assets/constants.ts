2d6f29cd81f79bcdfef2bdec3daa8d20b8390de2
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-06-07 20:48:42 +0200

    chore: point to prod

--- packages/docs/stories/assets/constants.ts
+++ packages/docs/stories/assets/constants.ts
@@ -7,18 +7,19 @@ export const codes = {
   doNotTrack: 'BOTT17OZFFFFFF000000XXXX'
 }
 
-export const clConfig = {
-  clientId: '2oh-47l4PN_yb8p4mp7AxUsb-Ue3e7tBqteoZcQM-nU',
-  slug: 'drop-in-js-stg',
-  scope: 'market:545',
-  debug: 'all',
-  domain: 'commercelayer.co'
-}
-
-// // prd
+// // stg
 // export const clConfig = {
-//   clientId: 'kuSKPbeKbU9LG9LjndzieKWRcfiXFuEfO0OYHXKH9J8',
-//   slug: 'drop-in-js',
-//   scope: 'market:11709',
-//   debug: 'all'
+//   clientId: '2oh-47l4PN_yb8p4mp7AxUsb-Ue3e7tBqteoZcQM-nU',
+//   slug: 'drop-in-js-stg',
+//   scope: 'market:545',
+//   debug: 'all',
+//   domain: 'commercelayer.co'
 // }
+
+// prd
+export const clConfig = {
+  clientId: 'kuSKPbeKbU9LG9LjndzieKWRcfiXFuEfO0OYHXKH9J8',
+  slug: 'drop-in-js',
+  scope: 'market:11709',
+  debug: 'all'
+}
