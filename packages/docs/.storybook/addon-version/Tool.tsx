fcbdb63ae3f3386187fe6244e90dc08a79c01169
Marco Montalbano <marco.montalbano@commercelayer.io>
2025-01-30 10:11:01 +0100

    v2.10.0

--- packages/docs/.storybook/addon-version/Tool.tsx
+++ packages/docs/.storybook/addon-version/Tool.tsx
@@ -15,7 +15,7 @@ export const Tool = () => {
           window.open(LINK_URL, '_blank')
         }}
       >
-        {/* DO NOT REMOVE - replace version */}v2.9.0
+        {/* DO NOT REMOVE - replace version */}v2.10.0
       </IconButton>
     </>
   )
--- replace-jsdelivr.js
+++ replace-jsdelivr.js
@@ -6,7 +6,7 @@
  * 
  * @example https://cdn.jsdelivr.net/npm/@commercelayer/drop-in.js@2/dist/drop-in/drop-in.esm.js
  * @example `drop-in.js@2`
- * @example `drop-in.js@2.9.0`
+ * @example `drop-in.js@2.10.0`
  */
 
 import { replaceInFileSync } from 'replace-in-file'
