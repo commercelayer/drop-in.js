0337f11d48412e02a6ac22143eb7d5e7bc9d74ce
Marco Montalbano <marco.montalbano@commercelayer.io>
2025-09-23 11:25:52 +0200

    v2.18.0

--- packages/docs/.storybook/addon-version/Tool.tsx
+++ packages/docs/.storybook/addon-version/Tool.tsx
@@ -15,7 +15,7 @@ export const Tool = () => {
           window.open(LINK_URL, "_blank")
         }}
       >
-        {/* DO NOT REMOVE - replace version */}v2.17.0
+        {/* DO NOT REMOVE - replace version */}v2.18.0
       </IconButton>
     </>
   )
